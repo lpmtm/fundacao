e94ab75d454d8ef5566f58df604a5f5a7d7d02fd
caroline lopes <161317565+lpmtm@users.noreply.github.com>
2025-09-22 16:32:40 -0300

    Update fundacao.sql

--- SQL/fundacao.sql
+++ SQL/fundacao.sql
@@ -1,46 +1,44 @@
-CREATE SCHEMA IF NOT EXISTS fundacao;
+--Projeto físico
 
-CREATE TABLE fundacao.aluno (		--populada 
+CREATE TABLE fundacao.aluno (	
     id_aluno INT PRIMARY KEY,
     nome_aluno VARCHAR(100) NOT NULL,
     email_aluno VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE fundacao.professores (		--populada 
+CREATE TABLE fundacao.professores (		
     id_professor INT PRIMARY KEY,
     email_professor VARCHAR(100) NOT NULL,
     nome_professor VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE fundacao.patrocinador (		--populada 
+CREATE TABLE fundacao.patrocinador (		 
     id_patrocinador INT PRIMARY KEY,
     nome_patrocinador VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE fundacao.curso (				--populada
+CREATE TABLE fundacao.curso (				
     id_curso INT PRIMARY KEY,
     duracao_curso INTERVAL NOT NULL,
 	nome_curso VARCHAR(100) NOT NULL,
-	nivel_curso VARCHAR(100) NOT NULL,
-    id_aluno INT REFERENCES fundacao.aluno(id_aluno),
-    id_professor INT REFERENCES fundacao.professores(id_professor)
+	nivel_curso VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE fundacao.eventos (			--populada
+CREATE TABLE fundacao.eventos (			
     id_eventos INT PRIMARY KEY,
 	nome_evento VARCHAR(100) NOT NULL,
     data_eventos DATE NOT NULL,
     id_curso INT REFERENCES fundacao.curso(id_curso)
 );
 
-CREATE TABLE fundacao.avaliacao (		--populada
+CREATE TABLE fundacao.avaliacao (		
     id_avaliacao INT PRIMARY KEY,
     nota_avaliacao INT NOT NULL,
     id_curso INT REFERENCES fundacao.curso(id_curso),
     id_aluno INT REFERENCES fundacao.aluno(id_aluno)
 );
 
-CREATE TABLE fundacao.boletim (			--populada
+CREATE TABLE fundacao.boletim (			
     id_boletim INT PRIMARY KEY,
     frequencia_boletim INT NOT NULL,
     desempenho_boletim NUMERIC NOT NULL,
@@ -49,7 +47,7 @@ CREATE TABLE fundacao.boletim (			--populada
     id_aluno INT REFERENCES fundacao.aluno(id_aluno)
 );
 
-CREATE TABLE fundacao.certificado (		--populada
+CREATE TABLE fundacao.certificado (		
     id_certificado INT PRIMARY KEY,
     data_conclusao DATE NOT NULL,
     id_curso INT REFERENCES fundacao.curso(id_curso),
@@ -62,13 +60,6 @@ CREATE TABLE fundacao.ministra_curso (
     id_curso INT REFERENCES fundacao.curso(id_curso),
     id_professor INT REFERENCES fundacao.professores(id_professor),
     PRIMARY KEY (id_curso, id_professor)
-);
-
--- Tabela de ligação para o relacionamento "realiza" (cursos-eventos)
-CREATE TABLE fundacao.realiza_evento (
-    id_curso INT REFERENCES fundacao.curso(id_curso),
-    id_eventos INT REFERENCES fundacao.eventos(id_eventos),
-    PRIMARY KEY (id_curso, id_eventos)
 );
 
 -- Tabela de ligação para o relacionamento "organiza" (eventos-patrocinador)
@@ -84,26 +75,3 @@ CREATE TABLE fundacao.aluno_curso (
     id_aluno INT REFERENCES fundacao.aluno(id_aluno),
     PRIMARY KEY (id_curso, id_aluno)
 );
-
--- Tabela de ligação para o relacionamento "tem" (aluno-boletim)
-CREATE TABLE fundacao.aluno_boletim (
-    id_aluno INT REFERENCES fundacao.aluno(id_aluno),
-    id_boletim INT REFERENCES fundacao.boletim(id_boletim),
-    PRIMARY KEY (id_aluno, id_boletim)
-);
-
--- Tabela de ligação para o relacionamento "tem" (aluno-certificado)
-CREATE TABLE fundacao.aluno_certificado (
-    id_aluno INT REFERENCES fundacao.aluno(id_aluno),
-    id_certificado INT REFERENCES fundacao.certificado(id_certificado),
-    PRIMARY KEY (id_aluno, id_certificado)
-);
-
--- Tabela de ligação para o relacionamento "tem" (aluno-avaliacao)
-CREATE TABLE fundacao.aluno_avaliacao (
-    id_aluno INT REFERENCES fundacao.aluno(id_aluno),
-    id_avaliacao INT REFERENCES fundacao.avaliacao(id_avaliacao),
-    PRIMARY KEY (id_aluno, id_avaliacao)
-);
-
---as consultas estão no outro doc
